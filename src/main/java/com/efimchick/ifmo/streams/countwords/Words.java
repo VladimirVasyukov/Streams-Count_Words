c3b2ac272c6946a2a36b3ae18a13a7f47de9e420
Vladimir Vasyukov <vladimir.vasyukov7@gmail.com>
2021-12-04 00:09:39 +0300

    Rebuilding the project

--- src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/streams/countwords/Words.java
@@ -1,47 +1,69 @@
 package com.efimchick.ifmo.streams.countwords;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-/**
- * 08 - Java Lambdas & Streams - Vladimir_Vasyukov
- */
-
 public class Words {
+    private static final Pattern REGEX_PATTERN = Pattern.compile("[^\\p{Alpha}]", Pattern.UNICODE_CHARACTER_CLASS);
     private static final String TEXT_FILE_DELIMITER = "\\s";
-    private static final String REGEX_DELIMITER =
-        "([\\u0000-\\u0040\\u005B-\\u0060\\u007B-\\u00BF\\u02B0-\\u036F\\u00D7\\u00F7\\u2000-\\u2BFF])+";
-    private static final String EMPTY_SYM = " ";
-    private static final int MIN_WORD_LENGTH = 4;
     private static final int MIN_WORD_COUNT = 10;
+    private static final int MIN_LENGTH_WORD = 4;
+    private static final String SPACE = " ";
+    private static final String DASH = " - ";
+    private static final String NEW_LINE = "\n";
 
     public String countWords(List<String> lines) {
-        String[] strings = lines.toString().replaceAll(REGEX_DELIMITER, EMPTY_SYM).split(TEXT_FILE_DELIMITER);
-        Map<String, WordStore> map = Arrays.stream(strings)
-            .filter(s -> s.length() >= MIN_WORD_LENGTH)
-            .map(String::toLowerCase)
-            .collect(Collectors.toMap(
-                k -> k,
-                WordStore::new,
-                (wordStore1, wordStore2) -> mergeWordCounts(wordStore1)));
-        List<WordStore> wordStoreList = new ArrayList<>(map.values());
-        return applyFilters(wordStoreList);
+        Map<String, Integer> countWordsInMap = countWordsInMap(getWordsFromText(lines));
+        Map<String, Integer> wordsMapForSort = filterWordsByMinFrequency(countWordsInMap);
+        Set<Map.Entry<String, Integer>> sortedWordsByFrequencySet = sortHashMap(wordsMapForSort);
+        return createResultString(sortedWordsByFrequencySet);
     }
 
-    public WordStore mergeWordCounts(WordStore wordStore1) {
-        wordStore1.incrementCount();
-        return wordStore1;
+    private List<String> getWordsFromText(List<String> text) {
+        String lowerCaseText = text
+            .stream()
+            .map(String::toLowerCase).collect(Collectors.joining(SPACE));
+        String[] textAsArray = REGEX_PATTERN.matcher(lowerCaseText).replaceAll(SPACE).split(TEXT_FILE_DELIMITER);
+        return Arrays
+            .stream(textAsArray)
+            .collect(Collectors.toList());
     }
 
-    public String applyFilters(Collection<WordStore> list) {
-        return list.stream()
-            .filter(o -> o.getCount() >= MIN_WORD_COUNT)
-            .sorted()
-            .map(Object::toString)
-            .collect(Collectors.joining("\n"));
+    private Map<String, Integer> countWordsInMap(List<String> wordsFromText) {
+        return wordsFromText
+            .stream()
+            .collect(Collectors.toMap(Function.identity(), v -> 1, Integer::sum));
+    }
+
+    private Map<String, Integer> filterWordsByMinFrequency(Map<String, Integer> wordsMap) {
+        return wordsMap
+            .entrySet()
+            .stream()
+            .filter(entry -> entry.getKey().length() >= MIN_LENGTH_WORD)
+            .filter(entry -> entry.getValue() >= MIN_WORD_COUNT)
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private Set<Map.Entry<String, Integer>> sortHashMap(Map<String, Integer> unsortedMap) {
+        return unsortedMap
+            .entrySet()
+            .stream()
+            .sorted(Map.Entry.<String, Integer>comparingByValue(Comparator.reverseOrder())
+                .thenComparing(Map.Entry.comparingByKey()))
+            .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
+    private String createResultString(Set<Map.Entry<String, Integer>> sortedWordsByFrequencySet) {
+        return sortedWordsByFrequencySet
+            .stream()
+            .map(entry -> entry.getKey() + DASH + entry
+            .getValue()).collect(Collectors.joining(NEW_LINE));
     }
 }
